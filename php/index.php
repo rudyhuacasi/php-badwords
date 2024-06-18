c4721a3f4ad91befc98275092346f33438c7c680
rudyhuacasi <alexisitaliano16@gmail.com>
2024-06-18 22:54:16 +0200

    {UPD} added package bootstrap

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- css bootstrap -->
+    <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="bg-primary">ciao</div>
+    </header>
+    
+    <!-- <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script> -->
+</body>
+</html>
