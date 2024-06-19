52c5888fb6dda646d245d13c6b62f8a713ebafdc
rudyhuacasi <alexisitaliano16@gmail.com>
2024-06-19 07:25:07 +0200

    {UPD} added first file (input richiesta di informazione)

--- php/index.php
+++ php/index.php
@@ -8,9 +8,26 @@
     <title>Document</title>
 </head>
 <body>
-    <header>
-        <div class="bg-primary">ciao</div>
+    <header class=" bg-primary-subtle">
+        <div class="container text-center p-3">
+            <h1><em class="border-bottom border-dark">Form PHP</em></h1>
+        </div>
     </header>
+    <main>
+        <div class="container">
+            <form action="ricevo.php" method="get">
+                <div class="m-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                    <textarea class="form-control" name="paragrafo" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div class="m-3">
+                    <label for="exampleFormControlInput1" class="form-label">Parola Censurata</label>
+                    <input type="text" name="censura" class="form-control" id="exampleFormControlInput1">
+                </div>
+                <button type="submit" class="btn btn-outline-danger m-3">Invio al server</button>
+            </form>
+        </div>
+    </main>
     
     <!-- <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script> -->
 </body>
